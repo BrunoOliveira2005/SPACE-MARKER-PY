c9d5fc66c4729ff8414400614f1280f0adc7d12f
Bruno Gonçalves de Oliveira <bruno.pf2005@gmail.com>
2024-06-15 14:15:00 -0300

    inicio

---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+import os 
+from tkinter import simpledialog
+
+pygame.init()
+
+
+clock = pygame.time.Clock()
+tamanho  = (1280,750)
+tela = pygame.display.set_mode(tamanho)
+icone = pygame.image.load("assets/icone.png")
+pygame.display.set_icon(icone)
+pygame.display.set_caption("Space Marker")
+fundo = pygame.image.load("assets/fundo.jpg")
+fonte = pygame.font.SysFont("comicsans",100)
+
+
+
+
+branco  = (255,255,255)
+preto = (0,0,0)
+
+
+estrelas = []
+
+
+def criarEstrelas():
+    posicaoMouse = pygame.mouse.get_pos()
+    estrelas.append(posicaoMouse)
+    nomeEstrela = simpledialog.askstring("Space Marker","Nome da Estrela:")
+    idEstrela = fonte.render(nomeEstrela,True,branco)
+    tela.blit(idEstrela,(posicaoMouse))
+    for posicao in estrelas: 
+            pygame.draw.circle(tela,branco,posicao,3,0)
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            quit()
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            criarEstrelas()
+
+
+
+    #tela.blit(fundo,(0,0))
+
+
+
+
+
+    pygame.display.update()
+    clock.tick(60)
